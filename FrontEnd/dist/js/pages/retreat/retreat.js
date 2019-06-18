8427cb86d7c991f2924e7abf71736cd8b3b5b92b
Yasin <yasinzhou@outlook.com>
2019-06-18 01:24:22 -0500

    retreat

--- FrontEnd/dist/js/pages/retreat/retreat.js
+++ FrontEnd/dist/js/pages/retreat/retreat.js
@@ -1,4 +1,19 @@
-
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
 $(document).ready(function() {
     "use strict"
     $('.select2').select2();
@@ -12,5 +27,20 @@ $(document).ready(function() {
         $('#studenIDGroup').addClass("has-error");
         $('#studenIDError').html("Student ID can't be empty");
         $('#studenIDError').show();
+        let data = JSON.stringify($("#retreatForm").serializeObject());
+        console.log(data);
+        $.ajax({
+            type: "post",
+            url: 'http://localhost:8080/retreat/save',
+            data: data,
+            contentType: 'application/json',
+            dataType: "json",
+            success: function (response) {
+                console.log('success');
+            },
+            error: function(errors){
+                console.log(errors.responseJSON);
+            }
+        })
     });
 });
