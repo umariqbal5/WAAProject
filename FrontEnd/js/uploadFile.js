4e70c1d3a7a78fbcc269eee02e907492165b1aaa
duynga <loduynga@gmail.com>
2019-06-19 14:58:54 -0500

    uploadFile frontend

--- FrontEnd/js/uploadFile.js
+++ FrontEnd/js/uploadFile.js
@@ -1,108 +1,53 @@
-const readFile = async (file) => {
+var lines = [];
+
+function previewText() {
+    var inputFile = document.getElementById('input-file');
     var reader = new FileReader();
-     reader.onload = function (evt) {
+    reader.onload = function (evt) {
         var textContents = evt.target.result;
         lines = textContents.split(/\n/);
-
-
         lines = jQuery.unique(lines);
-        var objects = [];
-        var cnt = 0;
-
-        for (const value of lines) {
-            values = value.split(",");
-            var myObject = new Object();
-            if (values.length == 4) {
-                myObject.id = values[0];
-                myObject.date = values[1];
-                myObject.timeSlot = values[2];
-                myObject.location = values[3];
-            } else if (values.length == 3) {
-                var now = new Date(values[0]);
-                var dateString = moment(now).format('MM/DD/YY');
-                myObject.date = dateString;
-                myObject.timeSlot = 'AM';
-                myObject.location = 'DB';
-                myObject.student = new Object();
-                myObject.student.registrationNumber = values[1];
-                myObject.student.name = values[2];
-            }
-            objects.push(myObject)
-            if (objects.length == 100) {
-                cnt++;
-                console.log("cnt: " + cnt);
-                 makeAjaxCall(objects);
-                objects=[];
-            }
-        }
-        if (objects.length != 0) {
-            makeAjaxCall(objects);
-        }
+        // console.log(lines);
     };
-    reader.readAsText(file);
+    reader.readAsText(inputFile.files[0]);
+};
 
+const readFile = async () => {
+    var objects = [];
+    var cnt = 0;
 
-    // const nums = [1,2,3,4,5,];
-    // let tr = [];
-    // for (const num of nums) {
-    //     tr.push(num);
-    //     if(tr.length==2){
-    //         const result = await returnNum(tr);
-    //         console.log("return back " +result);
-    //         tr=[];
-    //     }
-    // }
-    // if(tr.length!=0){
-    //     const result =  returnNum(tr);
-    // }
-    // console.log('after forEach');
+    for (const value of lines) {
+        values = value.split(",");
+        var myObject = new Object();
+        if (values.length == 4) {
+            myObject.id = values[0];
+            myObject.date = values[1];
+            myObject.timeSlot = values[2];
+            myObject.location = values[3];
+        } else if (values.length == 3) {
+            var now = new Date(values[0]);
+            var dateString = moment(now).format('MM/DD/YY');
+            myObject.date = dateString;
+            myObject.timeSlot = 'AM';
+            myObject.location = 'DB';
+            myObject.student = new Object();
+            myObject.student.registrationNumber = values[1];
+            myObject.student.name = values[2];
+        }
+        objects.push(myObject)
+        if (objects.length == 100) {
+            cnt++;
+            console.log("cnt: " + cnt);
+            await makeAjaxCall(objects);
+            objects = [];
+        }
+    }
+    if (objects.length != 0) {
+        await makeAjaxCall(objects);
+    }
 }
-//  function readFile(file) {
-//     var reader = new FileReader();
-//     reader.onload = function (evt) {
-//         var textContents = evt.target.result;
-//         lines = textContents.split(/\n/);
-//
-//
-//         lines = jQuery.unique(lines);
-//         var objects = [];
-//         var cnt = 0;
-//         $.each(lines, function (index, value) {
-//             // console.log(index + ": " + value );
-//             values = value.split(",");
-//             var myObject = new Object();
-//             if (values.length == 4) {
-//                 myObject.id = values[0];
-//                 myObject.date = values[1];
-//                 myObject.timeSlot = values[2];
-//                 myObject.location = values[3];
-//             } else if (values.length == 3) {
-//                 var now = new Date(values[0]);
-//                 var dateString = moment(now).format('MM/DD/YY');
-//                 myObject.date = dateString;
-//                 myObject.timeSlot = 'AM';
-//                 myObject.location = 'DB';
-//                 myObject.student = new Object();
-//                 myObject.student.registrationNumber = values[1];
-//                 myObject.student.name = values[2];
-//             }
-//             objects.push(myObject)
-//             if (objects.length == 100) {
-//                 cnt++;
-//                 console.log("cnt: " + cnt);
-//                  makeAjaxCall(objects);
-//                 objects=[];
-//             }
-//         });
-//         if (objects.length != 0) {
-//             makeAjaxCall(objects);
-//             objects=[];
-//         }
-//     };
-//     reader.readAsText(file);
-// }
 
- async function makeAjaxCall(objects) {
+async function makeAjaxCall(objects) {
     objects = Array.from(new Set(objects.map(JSON.stringify))).map(JSON.parse);
     var data = JSON.stringify(objects);
     // var data = JSON.stringify($("#employeeForm").serializeFormJSON());
@@ -125,10 +70,7 @@ const readFile = async (file) => {
 }
 
 $(document).ready(function () {
-
     $('#read-file').click(function (event) {
-        var inputFile = document.getElementById('input-file');
-        // var inputFile = $('#input-file').val();
-        readFile(inputFile.files[0]);
+        readFile();
     });
 });
