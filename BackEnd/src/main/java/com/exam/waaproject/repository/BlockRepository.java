639ddd833ad9e38ac716f7d83a2b9cc41a52f144
duynga <loduynga@gmail.com>
2019-06-20 03:35:34 -0500

    block generate

--- BackEnd/src/main/java/com/exam/waaproject/repository/BlockRepository.java
+++ BackEnd/src/main/java/com/exam/waaproject/repository/BlockRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface BlockRepository extends CrudRepository<Block, Long> {
 
-    @Query("Select b from Block b Where id >= :entryBlockId")
+    @Query("Select b from Block b Where id >= ?1")
     public List<Block> findBlocksByEntryBlockId(Long entryBlockId);
+
+    @Query("Select b from Block b Where  ?1 between startDate and endDate")
+    public Block findBlockByRange(Date date);
 
 }
